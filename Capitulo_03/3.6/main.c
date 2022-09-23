8680344a4edc3b738d860650af8d6613766e52de
Renan Aquino <renan.dowglasaquino@hotmail.com>
2022-09-23 18:09:55 -0300

    Atividade 3.6

---
+++ Capitulo_03/3.6/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+int main() {
+    char e[32];
+    char *posfix;
+    int y;
+    int x;
+    int n;
+    Pilha *p = pilha(32);
+
+    printf("Infixa: ");
+    fgets(e,31,stdin);
+    posfix = posfixa(e);
+    printf("Posfixa: %s\n", posfix);
+
+    for (int i = 0; posfix[i]; i++){
+        if (isalpha(posfix[i])){
+            push(posfix[i],p);
+        } else {
+            y = pop(p);
+            x = pop(p);
+            switch(posfix[i]){
+                case '~':
+                    push(y+x,p);
+                    break;
+                case '&':
+                    push(y-x,p);
+                    break;
+                case '|':
+                    push(y*x,p);
+                    break;
+            }
+        }
+    }
+
+    printf("Resultado: %s",desempilha(p));
+
+    return 0;
+}
