50005c05c2fcefdfffaf207c24bed51b40611f6c
renanaquinno <49573650+renanaquinno@users.noreply.github.com>
2022-09-27 17:48:42 -0300

    Add files via upload

---
+++ complete_o_codigo/fila_complete_o_codigo.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+class No{
+	public:
+		char nome;
+		No *prox;
+		No(char n){
+			nome = n;
+			prox = NULL;
+		}
+};
+
+class Fila{
+	public:
+		No *inicio;
+		No *fim;
+
+		Fila(){
+			inicio = NULL;
+			fim = NULL;
+		}
+
+		void append(char n){
+            No *tmp = new No(n);
+            tmp->nome = n;
+            tmp->prox = NULL;
+
+            if(inicio == NULL) {
+                inicio = tmp;
+                fim = tmp;
+            }   else {
+                fim->prox = tmp;
+                fim = fim->prox;
+            }
+
+            printf("Adicionado!");
+		}
+
+		char pop(class No** inicio){
+            if (*inicio == NULL) {
+                return 's';
+            } else {
+                return 'o';
+            }
+
+        //No(inicio)* inicio = *inicio;
+        //char result = inicio->nome;
+
+        //(*inicio) = (*inicio)->prox;
+
+        //free(inicio);
+
+        //return result;
+
+		}
+
+		bool isEmpty(){
+			return (inicio == NULL);
+		}
+
+	//    public void popAll(){
+
+
+//		}
+
+};
+
+int main(){
+
+    Fila a;
+    a.append('a');
+    a.pop();
+
+}
